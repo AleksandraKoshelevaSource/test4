ed417697634dafc7e2b78fb7f3f3abc2c7d7a335
Александра <aleksandralubenchenko@mail.ru>
2023-01-16 16:20:09 +0300

    s2

--- Test4/Form1.cs
+++ Test4/Form1.cs
@@ -13,9 +13,12 @@ namespace Test4
     public partial class Form1 : Form
     {
         string s = "TestString!";
+        string s2 = "TestString2!";
         public Form1()
         {
             InitializeComponent();
         }
+
+        public string S2 { get => s2; set => s2 = value; }
     }
 }
